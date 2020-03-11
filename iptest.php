11ab0d1b29b0c135a46055deb2f6303ae4c8c9b0
YS_Neko <41314905+YS-Neko@users.noreply.github.com>
2020-03-11 20:19:07 +0800

    增加了通过get参数改变探测链接名的功能

--- iptest.php
+++ iptest.php
@@ -1,6 +1,6 @@
 <head>
 	<meta itemprop="name" content="xml卡片标题" />
 	<meta itemprop="description" content="xml卡片描述" />
-	<meta itemprop="image" content="iptest.php文件链接" />
+	<meta itemprop="image" content="iptest.php文件链接?id=<?php echo $_GET["id"]; ?>" />   //此处注意链接带上参数id
 	<title>标题</title>
 </head>
